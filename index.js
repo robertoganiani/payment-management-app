24e09a8e5345c1bf6936218c02a0e483f85c9f68
Robert Oganiani <robertoganiani@gmail.com>
2018-04-15 12:05:54 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 8000
+
+app.use(express.static(`${__dirname}/app/build/default`))
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: '.' })
+})
+
+app.get('/test', (req, res) => {
+  res.status(200).send({ hello: 'from server' })
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
