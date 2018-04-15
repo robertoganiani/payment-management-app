b6462925f1f35ff9201c8225c2254d7a30bbcaca
Robert Oganiani <robertoganiani@gmail.com>
2018-04-15 23:16:33 +0400

    Add modal form

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8000
 
-app.use(express.static(`${__dirname}/app/build/default`))
+app.use(express.static(`${__dirname}/app`))
+// app.use(express.static(`${__dirname}/app/build/default`))
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: '.' })
