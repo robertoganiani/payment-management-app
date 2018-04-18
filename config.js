7c59e0968202d53e9b940466544835986ce19fdc
Robert Oganiani <robertoganiani@gmail.com>
2018-04-18 13:27:19 +0400

    Add config file, add some dependencies

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  jwtSecret: '&DClj3*HQ9iP^v35!UCQP$#q',
+  mongoUri: 'mongodb://heroku_v7n3xjcv:talimgmjvn5ug7197q0ursg9pl@ds135179.mlab.com:35179/heroku_v7n3xjcv'
+}
--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const expressJWT = require('express-jwt')
+const path = require('path')
+const mongoose = require('mongoose')
+
+const config = require('./config')
 
 const app = express()
+const router = express.Router()
 const PORT = process.env.PORT || 8000
+const MONGO_URI = config.mongoUri
 
-app.use(express.static(`${__dirname}/app`))
+mongoose.Promise = global.Promise
+mongoose.connect(MONGO_URI)
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab'))
+  .on('error', error => console.log('Error connecting to MongoLab: ', error))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'app')))
 // app.use(express.static(`${__dirname}/app/build/default`))
+app.use(
+  expressJWT({ secret: config.jwtSecret }).unless({
+    path: ['/signin']
+  })
+)
+app.use('/', router)
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: '.' })
@@ -15,5 +37,5 @@ app.get('/test', (req, res) => {
 })
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`)
+  console.log(`Server listening on port ${PORT}`)
 })
